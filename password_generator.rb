61b1389b47c4bc20336355ba4df9a6e0a8e44ede
Andras Horvath <han@log69.com>
2012-01-26 00:31:34 +0100

    sorting algorithms fully rewritten to avoid rerun when password is not suiteable

--- password_generator.rb
+++ password_generator.rb
@@ -9,43 +9,41 @@
 # that doesn't contain similar easily mixable chars
 # and can be used properly on english and hungarian keybords too
 # len means an integer of 4 or greater
+#
+# the rules are:
+# - min 1 lower case letter is necessary
+# - min 1 upper case letter is necessary
+# - min 1 number is necessary
 def get_password(len)
 
 	# all available chars to choose from
 	# "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	# exception chars
-	# "o|O|0|1|i|j|l|I|z|Z|y|Y|g|q|9"
+	# "o|O|0|Q|1|i|j|l|I|z|Z|y|Y|g|q|9"
 	# "z" and "y" are exceptions because of querty key maps
-	ch = "abcdefhkmnprstuvwxABCDEFGHJKLMNPQRSTUVWX2345678"
+	ch = "abcdefhkmnprstuvwxABCDEFGHJKLMNPRSTUVWX2345678".split("")
+	ch_low = "abcdefhkmnprstuvwx".split("")
+	ch_upp = "ABCDEFGHJKLMNPRSTUVWX".split("")
+	ch_num = "2345678".split("")
 
 	# pass should have at least 4 chars
 	if len < 4 then return "" end
 
-	while true
-		clow = 0
-		cupp = 0
-		cnum = 0
+	pass = ""
 
-		# get a sample
-		pass = ""
-		for i in (0..len-1)
-			x = rand(ch.length)
-			c = ch[x..x]
-			pass += c
+	# get 1 lower, 1 upper case and 1 number for sure
+	pass += ch_low[rand(ch_low.length)]
+	pass += ch_upp[rand(ch_upp.length)]
+	pass += ch_num[rand(ch_num.length)]
 
-			# low case, upper case or num?
-			if c >= "a" and c <= "z" then clow = 1 end
-			if c >= "A" and c <= "Z" then cupp = 1 end
-			if c >= "0" and c <= "9" then cnum = 1 end
-
-		end
-
-		# pass should have at least 1 low case and 1 upcase letter
-		# and 1 number too from the exception list
-		# if so, then success and exit
-		if clow == 1 and cupp == 1 and cnum == 1 then return pass end
-
+	# get the rest of the sample randomly
+	for i in (0..len-4)
+		c = ch[rand(ch.length)]
+		pass += c
 	end
+
+	# shuffle the order of chars in result
+	return pass.split("").shuffle.join
 end
 
 
@@ -55,46 +53,47 @@ end
 # len means an integer of 4 or greater
 #
 # the rules are:
+# - only 1 upper case letter is necessary
+# - only 1 number is necessary
+# - min 1 lower case letter is necessary
 # - max 2 vowels may follow each other if they're not identical except "ee"
 # - max 2 consonants may follow each other if they match
-# - max 1 upper case letter is allowed
-# - max 1 number is allowed
 def get_password_pron(len)
 
 	# all available chars to choose from
-	# "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	# "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ0123456789"
 	# exception chars
-	# "o|O|0|1|i|j|l|I|z|Z|y|Y|g|q|9"
+	# "o|O|0|Q|1|i|j|l|I|z|Z|y|Y|g|q|9"
 	# "z" and "y" are exceptions because of querty key maps
-	ch = "abcdefhkmnprstuvwxABCDEFGHJKLMNPQRSTUVWX2345678"
-	ch_vow = "aeuAEU"
-	ch_con = "bcdfhkmnprstvwxBCDFGHJKLMNPQRSTVWX"
-	ch_num = "2345678"
+	ch = "abcdefhkmnprstuvwxABCDEFGHJKLMNPRSTUVWX2345678".split("")
+	ch_low = "abcdefhkmnprstuvwx".split("")
+	ch_upp = "ABCDEFGHJKLMNPRSTUVWX".split("")
+	ch_num = "2345678".split("")
+	ch_vow = "aeu".split("")
+	ch_con = "bcdfhkmnprstvwx".split("")
 	ch_con_double = %w[ ch kh ph sh th ]
 
 	# pass should have at least 4 chars
 	if len < 4 then return "" end
 
+	pass = []
+
+	# get 1 number for sure
+	pass += [ch_num[rand(ch_num.length)]]
+
 	while true
 
-		# get a sample
-		pass = ""
-		cupp = 0
+		# get the rest of the sample randomly
 		ch_type_old = -1
-		ch2 = ""
+		ch_old = ""
 		num_vow = 0
 		num_con = 0
-		num_num = 0
 		first_run = 1
-		for i in (0..len-1)
+		for i in (0..len-2)
 
-			# choose a char type (vowel, consonant or number)
-			# don't choose number if there was one already
-			if num_num == 0
-				ch_type = rand(3)
-			else
-				ch_type = rand(2)
-			end
+			# choose a char type (vowel or consonant)
+			# we have number and upper case already
+			ch_type = rand(2)
 
 			# run it until 1 good char returns
 			ok = 0
@@ -104,13 +103,11 @@ def get_password_pron(len)
 				# vowels -----
 				when 0
 					# get random char
-					n = rand(ch_vow.length)
-					ch1 = ch_vow[n..n]
-					if cupp == 1 then ch1 = ch1.downcase end
+					ch = ch_vow[rand(ch_vow.length)]
 
 					# is it the first run on the first char?
 					if first_run == 1 then
-						pass += ch1
+						pass += [ch.upcase]
 						num_vow += 1
 						ok = 1
 					else
@@ -118,8 +115,8 @@ def get_password_pron(len)
 						if ch_type_old == 0
 							# max 2 vowels allowed after each other
 							# if they're not identical except "ee"
-							if num_vow < 2 and (ch1.downcase != ch2.downcase or ch1.downcase == "e")
-								pass += ch1
+							if num_vow < 2 and (ch != ch_old or ch == "e")
+								pass[pass.length-1] += ch
 								num_vow += 1
 								ok = 1
 							else
@@ -127,7 +124,7 @@ def get_password_pron(len)
 								ch_type = 1
 							end
 						else
-							pass += ch1
+							pass += [ch]
 							num_vow += 1
 							ok = 1
 						end
@@ -136,13 +133,11 @@ def get_password_pron(len)
 				# consonants -----
 				when 1
 					# get random char
-					n = rand(ch_con.length)
-					ch1 = ch_con[n..n]
-					if cupp == 1 then ch1 = ch1.downcase end
+					ch = ch_con[rand(ch_con.length)]
 
 					# is it the first run on the first char?
 					if first_run == 1 then
-						pass += ch1
+						pass += [ch.upcase]
 						num_con += 1
 						ok = 1
 					else
@@ -150,8 +145,8 @@ def get_password_pron(len)
 						if ch_type_old == 1
 							# max 2 consonants allowed after each other
 							# they must be the same or in the allowed match list
-							if num_con < 2 and (ch2 == ch1 or ch_con_double.include? ch2.downcase + ch1.downcase)
-								pass += ch1
+							if num_con < 2 and (ch_old == ch or ch_con_double.include? ch_old + ch)
+								pass[pass.length-1] += ch
 								num_con += 1
 								ok = 1
 							else
@@ -159,51 +154,55 @@ def get_password_pron(len)
 								ch_type = 0
 							end
 						else
-							pass += ch1
+							pass += [ch]
 							num_con += 1
 							ok = 1
 						end
 					end
 
-				# numbers -----
-				when 2
-					# get random char
-					n = rand(ch_num.length)
-					ch1 = ch_num[n..n]
-
-					pass += ch1
-					num_num += 1
-					ok = 1
 				end
 			end
 
 			first_run = 0
 			ch_type_old = ch_type
-			ch2 = ch1
-			
-			# char was an upper case?
-			if ("A".."Z").to_a.to_s.include? ch1 then cupp = 1 end
-		end
-
-
-		# check sample
-		clow = 0
-		cupp = 0
-		cnum = 0
-		for i in (0..len-1)
-			c = pass[i..i]
-
-			# low case, upper case or num?
-			if c >= "a" and c <= "z" then clow = 1 end
-			if c >= "A" and c <= "Z" then cupp = 1 end
-			if c >= "0" and c <= "9" then cnum = 1 end
+			ch_old = ch
 
 		end
 
-		# pass should have at least 1 low case and 1 upcase letter
-		# and 1 number too from the exception list
-		# if so, then success and exit
-		if clow == 1 and cupp == 1 and cnum == 1 then return pass end
+		# shuffle the order of chars of the result
+		# it must be done separately in groups of vowels and consonants
+		# so the same type won't get next to each other after mixing
+		pass_num = pass[0]
+		pass_ar1 = []
+		pass_ar2 = []
+		# start from char 1 because first char is a number
+		for i in (1..pass.length-1)
+			case i % 2
+			when 0
+				pass_ar1 += [pass[i]]
+			when 1
+				pass_ar2 += [pass[i]]
+			end
+		end
+		# shuffle them separately
+		pass_ar1.shuffle!
+		pass_ar2.shuffle!
+		# put them back to their places
+		pass2 = []
+		c1 = 0; c2 = 0;
+		for i in (1..pass.length-1)
+			case i % 2
+			when 0
+				pass2 += [pass_ar1[c1]]
+				c1 += 1
+			when 1
+				pass2 += [pass_ar2[c2]]
+				c2 += 1
+			end
+		end
+		# insert number randomly into somewhere
+		pass2.insert(rand(pass2.length+1), pass_num)
+		return pass2.join
 
 	end
 end

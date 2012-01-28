c0f7c5f9524bdd3a94cc154c85b1fe4d95fded3a
Andras Horvath <han@log69.com>
2012-01-28 10:12:19 +0100

    remove dependency of pidstat

--- statistics_system.rb
+++ statistics_system.rb
@@ -1,46 +1,65 @@
 #!/usr/bin/ruby
 # info: print overall system statistics
-# (also summarizes the values of separate processes with the same names)
-# depends: ruby, ps, pidstat (sysstat), acpi, pydf
-# license: GPLv3+
+#  summarizes the values of the separate processes with the same names
+#  cpu usage of processes show the used cpu since their start
+#  disk usage of processes show the average of read + written kilobytes
+#   since their start
+# platform: Linux only
+# depends: *acpi, *pydf (*optional dependencies)
+# license: GPLv3+ <http://www.gnu.org/licenses/gpl.txt>
 # Andras Horvath <mail@log69.com>
 
 
-# colorizing rutins
+# print colorized text
 def colorize(text, color_code) "\e[#{color_code}m#{text}\e[0m" end
 def red   (text); colorize(text, 31); end
 def green (text); colorize(text, 32); end
 def yellow(text); colorize(text, 33); end
 def blue  (text); colorize(text, 34); end
 
-# uptime
-system("clear")
-puts red("Uptime: " + `uptime`)
-
-# battery
-if not system("which acpi >/dev/null")
-	puts "error: acpi command missing"; puts
-else
-bar = 40
-out = `acpi -V 2>/dev/null`
-bat = out.scan(/[0-9]+\%/)
-if bat != [] then
-	len = bat[0].match(/[0-9]+/).to_s.to_i
-	if len < 0   then len = 0   end
-	if len > 100 then len = 100 end
-	text = "Battery: [" + "#" * (len * bar / 100) + \
-		"." * ((100 - len) * bar / 100) + "]"
-	if len >= 20 then puts green(text)
-	else              puts red(text)
+# check the existence of an executable
+def which(cmd)
+	paths = ENV["PATH"].split(":")
+	paths.push("/usr/local/bin", "/usr/bin", "/bin").uniq!
+	paths.each do |dir|
+		file = dir + "/" + cmd
+		if File.exists?(file) and File.executable?(file)
+			return true end
 	end
-	puts out.scan(/^(?!Cooling).*/)
-	puts
-end
+	return
 end
 
-# memory
+
+# uptime
+# get uptime of system in seconds
+f = File.open("/proc/uptime")
+text = f.read; f.close
+sys_uptime = text.split[0].to_i
+sys_uptime_day  = sys_uptime / 60 / 60 / 24
+sys_uptime_left = sys_uptime - sys_uptime_day * 60 * 60 * 24
+sys_uptime_hour = sys_uptime_left / 60 / 60
+sys_uptime_min  = (sys_uptime_left - sys_uptime_hour * 60 * 60) / 60
+# print current time
+print Time.now.to_s + ", up "
+# print uptime
+# print days of uptime
+if sys_uptime_day > 0
+	print sys_uptime_day.to_s
+	if sys_uptime_day > 1 then print " days "
+	else print " day " end
+end
+# print hours of uptime
+print sys_uptime_hour.to_s + ":" + sys_uptime_min.to_s + ", "
+# get load of system
+f = File.open("/proc/loadavg")
+text = f.read; f.close
+# print load average
+print "load " + (text.split[0..2] * ", ").to_s
+puts; puts
+
+# system memory
 print blue("Memory (MB): ")
-file = File.open("/proc/meminfo", "rb")
+file = File.open("/proc/meminfo", "r")
 text = file.read
 file.close
 text = text.gsub(/ kB$/, "")
@@ -67,134 +86,159 @@ print "Free ("     + mem_free.to_s     + ") "
 print "Swap ("     + mem_swap.to_s     + ") "
 print "\n\n"
 
-# mem usage
-if not system("which ps >/dev/null")
-	puts "error: ps command missing"; puts
-else
-puts red("Memory usage (MB):")
-text = `ps -eo comm,rss`
-texta = text.split("\n")[1..text.split("\n").length-1]
-textb = []
-for i in (0..(texta.length-1))
-	textb[i] = texta[i].split[0], \
-		texta[i].split[1].to_i / 1024
+# get process infos
+proc_list = []
+# search for pid dirs in /proc
+Dir.foreach("/proc") do |file|
+	path = "/proc/" + file
+	if File.directory?(path) and file =~ /^\d+$/
+		
+		p_name = ""
+		p_cpu  = 0
+		p_disk = 0
+		p_mem  = 0
+
+		# get name and cpu usage
+		if File.readable?(path + "/stat")
+			f = File.open(path + "/stat", "r")
+			text = f.read.split; f.close
+
+			p_name = text[1][1..-2]
+
+			p_cpu = 0
+			text[13, 2].each {|x| p_cpu += x.to_i}
+		end
+
+
+		# get disk usage
+		if File.readable?(path + "/io")
+			f = File.open(path + "/io", "r")
+			text = f.read; f.close
+			p_disk  = text.match("^read_bytes\:.*" ).to_s.match("[0-9]+").to_s.to_i
+			p_disk += text.match("^write_bytes\:.*").to_s.match("[0-9]+").to_s.to_i
+		end
+
+		# get mem usage
+		if File.readable?(path + "/status")
+			f = File.open(path + "/status", "r")
+			text = f.read; f.close
+			p_mem = text.match("^VmRSS\:.*").to_s.match("[0-9]+").to_s.to_i
+		end
+		
+#		puts p_name + " / cpu = " + p_cpu.to_s + " / mem = " + p_mem.to_s + " / disk = " + p_disk.to_s
+		
+		if p_name != ""
+			proc_list += [[p_name, p_cpu, p_mem, p_disk]] end
+	end
 end
-textb = textb.sort
-textc = []
+
+# sort list by name first
+proc_list.sort!
+
+# migrate duplicates of process info by name
+proc_new = []
 t1 = ""; t2 = ""
-n1 = 0; n2 = 0
 c1 = 0; c2 = 0
 while true
-	t1 = textb[c1][0]
-	n1 = textb[c1][1].to_i
+	t1 = proc_list[c1]
 	if t2 == "" then
-		t2 = t1; n2 = n1
-		textc[c2] = n2, t2
+		proc_new[c2] = t1
+		t2 = t1
 	else
-		if t1 == t2 then
-			n2 += n1
-			textc[c2][0] = n2
+		if t1[0] == t2[0] then
+			proc_new[c2][1] += t1[1]
+			proc_new[c2][2] += t1[2]
+			proc_new[c2][3] += t1[3]
 		else
 			c2 += 1
-			t2 = t1; n2 = n1
-			textc[c2] = n2, t2
+			proc_new[c2] = t1
+			t2 = t1
 		end
 	end
 	c1 += 1
-	if c1 >= textb.length then break end
-end
-for i in textc.sort.reverse[0..4]
-	print i[1] + " (" + i[0].to_s + ") "
-end
-puts; puts
+	if c1 >= proc_list.length then break end
 end
 
-# disk usage
-if not system("which pidstat >/dev/null")
-	puts "error: pidstat command missing"; puts
-else
-puts red("Disk usage (KB/s):")
-text = `pidstat -hd`
-texta = text.split("\n")[3, text.split("\n").length-1]
-textb = []
-for i in (0..(texta.length-1))
-	textb[i] = texta[i].split[5], \
-		texta[i].split[2].to_f + texta[i].split[3].to_f
-end
-textb = textb.sort
-textc = []
-t1 = ""; t2 = ""
-n1 = 0; n2 = 0
-c1 = 0; c2 = 0
-while true
-	t1 = textb[c1][0]
-	n1 = textb[c1][1].to_f
-	if t2 == "" then
-		t2 = t1; n2 = n1
-		textc[c2] = n2, t2
-	else
-		if t1 == t2 then
-			n2 += n1
-			textc[c2][0] = n2
-		else
-			c2 += 1
-			t2 = t1; n2 = n1
-			textc[c2] = n2, t2
-		end
-	end
-	c1 += 1
-	if c1 >= textb.length then break end
-end
-for i in textc.sort.reverse[0..4]
-	print i[1] + " (" + i[0].to_s + ") "
-end
-puts; puts
-end
 
-# cpu usage
-if not system("which pidstat >/dev/null")
-	puts "error: pidstat command missing"; puts
-else
+# -----------------
+# --- cpu usage ---
+# -----------------
+# get cpu list
+proc_cur = []
+for i in (0..proc_new.length-1)
+	proc_cur[i] = [proc_new[i][1], proc_new[i][0]]
+end
+# print cpu list
 puts red("CPU usage (%):")
-text = `pidstat -h`
-texta = text.split("\n")[3, text.split("\n").length-1]
-textb = []
-for i in (0..(texta.length-1))
-	textb[i] = texta[i].split[7], texta[i].split[5]
+for i in proc_cur.sort.reverse[0..4]
+	print i[1] + " (" + \
+		((i[0].to_f * 100 / sys_uptime).round.to_f / 100).to_s \
+		+ ") "
 end
-textb = textb.sort
-textc = []
-t1 = ""; t2 = ""
-n1 = 0; n2 = 0
-c1 = 0; c2 = 0
-while true
-	t1 = textb[c1][0]
-	n1 = textb[c1][1].to_f
-	if t2 == "" then
-		t2 = t1; n2 = n1
-		textc[c2] = n2, t2
-	else
-		if t1 == t2 then
-			n2 += n1
-			textc[c2][0] = n2
-		else
-			c2 += 1
-			t2 = t1; n2 = n1
-			textc[c2] = n2, t2
-		end
-	end
-	c1 += 1
-	if c1 >= textb.length then break end
+puts; puts
+
+# -----------------
+# --- mem usage ---
+# -----------------
+# get mem list
+proc_cur = []
+for i in (0..proc_new.length-1)
+	proc_cur[i] = [proc_new[i][2] / 1024, proc_new[i][0]]
 end
-for i in textc.sort.reverse[0..4]
+# print mem list
+puts red("Memory usage (MB):")
+for i in proc_cur.sort.reverse[0..4]
 	print i[1] + " (" + i[0].to_s + ") "
 end
 puts; puts
-end
 
-# disk usage
-if not system("which pydf >/dev/null")
+# ------------------
+# --- disk usage ---
+# ------------------
+# get disk list
+proc_cur = []
+for i in (0..proc_new.length-1)
+	proc_cur[i] = [proc_new[i][3] / 1024, proc_new[i][0]]
+end
+# print disk list
+puts red("Disk usage (KB/s):")
+for i in proc_cur.sort.reverse[0..4]
+	print i[1] + " (" + \
+		((i[0].to_f * 100 / sys_uptime).round.to_f / 100).to_s \
+		+ ") "
+end
+puts; puts
+
+
+# -------------------------
+# --- system disk usage ---
+# -------------------------
+if not which("pydf")
 	puts "error: pydf command missing"; puts
 else
 system("pydf"); puts
 end
+
+# ---------------
+# --- battery ---
+# ---------------
+if not which("acpi")
+	puts "warning: acpi command missing"; puts
+else
+bar = 40
+out = `acpi -V 2>/dev/null`
+bat = out.scan(/[0-9]+\%/)
+if bat != [] then
+	len = bat[0].match(/[0-9]+/).to_s.to_i
+	if len < 0   then len = 0   end
+	if len > 100 then len = 100 end
+	text = "Battery: [" + "#" * (len * bar / 100) + \
+		"." * ((100 - len) * bar / 100) + "]"
+	if len >= 20 then puts green(text)
+	else              puts red(text)
+	end
+	puts out
+	puts
+end
+end
+

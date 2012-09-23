bcf8511f08ad06958ef075b9ca4943146b556620
Andras Horvath <mail@log69.com>
2012-09-23 12:40:32 +0200

    add script

---
+++ btrfs_snapshot.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# create readonly snapshot of root dir
+# and delete oldest ones
+
+# snapshot dir
+DIR="/snapshot"
+
+# number of snapshots to leave for daily
+ND="7"
+# for monthly
+NM="3"
+
+# create daily snapshot
+mkdir -p "$DIR"
+btrfs sub snap -r / "$DIR"/`date +%Y%m%d%H%M%S`_daily
+
+# create monthly too if it doesn't exist
+if ! ls -1d "$DIR"/`date +%Y%m`*_monthly &>/dev/null; then
+   btrfs sub snap -r / "$DIR"/`date +%Y%m%d%H%M%S`_monthly
+fi
+
+# delete oldest ones for daily (n+1 for tail)
+ls -1rd "$DIR"/*_daily 2>/dev/null | \
+   tail -n+$(($ND+1)) | while read FF; do \
+      btrfs sub del "$FF"; done
+
+# delete for monthly too
+ls -1rd "$DIR"/*_monthly 2>/dev/null | \
+   tail -n+$(($NM+1)) | while read FF; do \
+      btrfs sub del "$FF"; done
+

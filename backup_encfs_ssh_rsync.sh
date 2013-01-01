b6ef75440fd39c15520604b8f482e7db4419ae0e
Andras Horvath <mail@log69.com>
2013-01-01 11:40:42 +0100

    bug fix: remove quotes from the name of excluded dirs

--- backup_encfs_ssh_rsync.sh
+++ backup_encfs_ssh_rsync.sh
@@ -40,7 +40,7 @@ then
 	# read args from 3rd only and store dir name with --exclude option of rsync
 	EXCLUDE_LIST=$(echo $@ | tr -s " " "\n" | tail -n+3 | while read DIR; do
 		N=$(encfsctl encode --extpass "echo $PASS" "$BACKUP_DIR" "$DIR")
-		echo -n " --exclude=\"$N\""
+		echo -n " --exclude=$N"
 	done)
 fi
 
